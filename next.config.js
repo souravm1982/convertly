4024a9504342850c7d2221d36d545114e6a0e509
Sourav M <sourav.man@gmail.com>
2026-04-05 10:16:46 -0700

    Fix Amplify deployment: remove macOS-specific FFmpeg, add build config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@aws-sdk/client-s3', '@aws-sdk/client-bedrock-runtime', 'fluent-ffmpeg']
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Exclude ffmpeg from client bundle
+      config.externals.push('fluent-ffmpeg');
+    }
+    return config;
+  }
+}
 
 module.exports = nextConfig
